135eba10dee66ea9c4a4c9b7fe9e4888afffae26
porkyt <fabyjesusrivas@gmail.com>
2022-03-18 20:57:00 +0000

    created query to create users table if it does not exist

---
+++ createTables/CreateUsersTable.go
@@ -0,0 +1,13 @@
+package createtables
+
+import "database/sql"
+
+// CreateUsersTable will create users table for database
+// if it does not exist
+func CreateUsersTable(db *sql.DB) {
+	var query string = "CREATE TABLE users;"
+	_, dataErr := db.Query(query)
+	if dataErr != nil {
+		panic(dataErr)
+	}
+}
