6c3db82875ab0a12c22f44e9fa4ca5bc567c14d0
porkyt <fabyjesusrivas@gmail.com>
2022-03-18 21:04:39 +0000

    added properties for table users

--- createTables/CreateUsersTable.go
+++ createTables/CreateUsersTable.go
@@ -5,7 +5,7 @@ import "database/sql"
 // CreateUsersTable will create users table for database
 // if it does not exist
 func CreateUsersTable(db *sql.DB) {
-	var query string = "CREATE TABLE users;"
+	var query string = "CREATE TABLE users(id SERIAL PRIMARY KEY, nickname VARCHAR(255) NOT NULL);"
 	_, dataErr := db.Query(query)
 	if dataErr != nil {
 		panic(dataErr)
